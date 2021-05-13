3dc7dcdbaf5c25429a1eede4f030d20fa0a02394
Aum Thanetanukun <chaloem.tha@outlook.com>
2021-05-13 20:46:48 +0700

    getMax

--- MyFirstC++Project/Source.cpp
+++ MyFirstC++Project/Source.cpp
@@ -4,16 +4,28 @@
 
 using namespace std;
 
-void sayHi(string name, int age);
+int getMax(int num1, int num2)
+{
+	if (num1>num2) {
+		return num1;
+	}
+	else {
+		return num2;
+	}
+}
 
 int main()
 {
-	sayHi("Aum", 26);
+	int num1;
+	int num2;
+
+	cout << "Please insert num1:" << endl;
+	cin >> num1;
+
+	cout << "Please insert num2:" << endl;
+	cin >> num2;
+
+	cout << "Max Number is: " + to_string(getMax(num1, num2));
 
 	return 0;
-}
-
-void sayHi(string name, int age)
-{
-	cout << "Hello " + name + ", you are " + to_string(age) + " years old." << endl;
 }
