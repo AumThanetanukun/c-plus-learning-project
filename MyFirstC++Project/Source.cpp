732b059879d138245ba5a8499928717be74cfc9b
Aum Thanetanukun <chaloem.tha@outlook.com>
2021-05-14 21:52:36 +0700

    2 arrays and nested loops

--- MyFirstC++Project/Source.cpp
+++ MyFirstC++Project/Source.cpp
@@ -4,53 +4,18 @@
 
 using namespace std;
 
-string getDayOfWeek(int dayNum) {
-	
-	switch (dayNum) {
-	case 0:
-		return "Sunday";
-		break;
-	case 1:
-		return "Monday";
-		break;
-	case 2:
-		return "Tuesday";
-		break;
-	case 3:
-		return "Wednesday";
-		break;
-	case 4:
-		return "Thursday";
-		break;
-	case 5:
-		return "Friday";
-		break;
-	case 6:
-		return "Saturday";
-		break;
-	default:
-		return "Invalid";
-	}
-}
-
 int main()
 {
-	//int num1;
-	//cout << "Please insert num1:" << endl;
-	//cin >> num1;
+	int numberGrid[3][2] = {
+		{1,2},
+		{3,4},
+		{5,6}
+	};
 
-	//while (num1 < 10) {
-	//	cout << "Result = " + getDayOfWeek(num1) << endl;
-	//	num1++;
-	//}
-	
-	int arr[] = { 1,2,3,4,5,6 };
-	cout << sizeof(arr) << endl;
-	cout << sizeof(arr[0]) << endl;
-	cout << sizeof(arr) / sizeof(arr[0]) << endl;
-	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
-		cout << arr[i] << endl;
-		cout << "Result = " + getDayOfWeek(i) << endl;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 2; j++) {
+			cout << numberGrid[i][j] << endl;
+		}
 	}
 
 	return 0;
