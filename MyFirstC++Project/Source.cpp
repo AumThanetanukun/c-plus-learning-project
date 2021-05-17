c735e3af0425870975dd7912df19dc3c2de495d2
Aum Thanetanukun <chaloem.tha@outlook.com>
2021-05-17 17:50:55 +0700

    Inheritance

--- MyFirstC++Project/Source.cpp
+++ MyFirstC++Project/Source.cpp
@@ -4,42 +4,35 @@
 
 using namespace std;
 
-class Movie {
-private:
-	string rating;
-
-	void setRating(string rating) {
-		if (rating == "G" || rating == "PG" || rating == "PG-13" || rating == "R" || rating == "NR") {
-			this->rating = rating;
-		}
-		else {
-			cout << "Rating input error for " + title + " => " + rating << endl;
-			this->rating = "NR"; // Not Rated
-		}
-	}
-
+class Chef {
 public:
-	string title;
-	string director;
-
-	Movie(string title, string director, string rating) {
-		this->title = title;
-		this->director = director;
-		setRating(rating);
+	void makeChicken() {
+		cout << "The chef makes yummy chicken" << endl;
 	}
-	string getRating() {
-		return rating;
+	void makeSalad() {
+		cout << "The chef makes salad" << endl;
+	}
+	void makeSpecialDish() {
+		cout << "The chef makes bbq ribs" << endl;
+	}
+};
+
+class ItalianChef : public Chef {
+public:
+	void makePasta() {
+		cout << "The chef makes pasta" << endl;
 	}
 };
 
 
 int main()
 {
-	Movie avengers("The Avengers", "Joss Whedon", "PG-13");
-	Movie ghost("The Ghost", "Unknown", "Dog");
+	Chef chef;
+	chef.makeChicken();
 
-	cout << "movie1: " << avengers.getRating() << endl;
-	cout << "movie2: " << ghost.getRating() << endl;
+	ItalianChef italianChef;
+	italianChef.makeChicken();
+	italianChef.makePasta();
 
 	return 0;
 }
