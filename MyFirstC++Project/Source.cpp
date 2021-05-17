72a362f6bd8f88e98809a8bb4ff6e060702afad2
Aum Thanetanukun <chaloem.tha@outlook.com>
2021-05-17 17:20:47 +0700

    contructor function

--- MyFirstC++Project/Source.cpp
+++ MyFirstC++Project/Source.cpp
@@ -9,23 +9,34 @@ public:
 	string title;
 	string author;
 	int pages;
+
+	Book() {
+		title = "no title";
+		author = "no auther";
+		pages = 0;
+		cout << "Creating Object... null" << endl;
+	}
+
+	Book(string title, string author, int pages) {
+		this->title = title;
+		this->author = author;
+		this->pages = pages;
+		cout << "Creating Object... " + title << endl;
+	}
 };
 
 
 int main()
 {
-	Book book1;
-	book1.title = "Harry Potter";
-	book1.author = "JK Rowling";
-	book1.pages = 500;
+	Book book1("Harry Potter", "A", 1);
+	Book book2("LOTR", "B", 2);
+	Book book3("News", "C", 3);
+	Book book4;
 
-	Book book2;
-	book2.title = "LOTR";
-	book2.author = "Unkown";
-	book2.pages = 700;
-
-	cout << book1.title << endl;
-	cout << book2.title << endl;
+	cout << "Book1: " << book1.title << endl;
+	cout << "Book2: " << book2.title << endl;
+	cout << "Book3: " << book3.title << endl;
+	cout << "Book4: " << book4.title << endl;
 
 	return 0;
 }
